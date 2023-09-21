77a5e43f78c90c3668b46f10edd1a7d6be804cb6
David F14 <davidfalue@gmail.com>
2023-09-22 01:15:46 +0200

    link & search

--- components/Card.vue
+++ components/Card.vue
@@ -1,23 +1,19 @@
 <script setup>
-    defineProps(["id", "title", "date", "poster", "type"]);
+defineProps(["id", "title", "date", "poster", "type"]);
 </script>
 
 <template>
     <div class="bg-black rounded-lg shadow-md overflow-hidden m-4 px-2">
-        <div class="p-1 py-4">
-            <h3 v-if="title" class="font-semibold text-lg mb-2 text-center text-white">{{ title }}</h3>
-            <div class="overflow-hidden mb-4">
-                <img v-if="poster" class="h-400 w-300 object-cover" 
-                :src="poster" 
-                alt="Placeholder Image" /> 
+        <NuxtLink :to="`${type}/${id}`">
+            <div class="p-1 py-4">
+                <h3 v-if="title" class="font-semibold text-lg mb-2 text-center text-white">{{ title }}</h3>
+                <div class="overflow-hidden mb-4">
+                    <img v-if="poster" class="h-400 w-300 object-cover" :src="poster" alt="Placeholder Image" />
+                </div>
+                <p class="text-gray-500 text-sm mb-2 px-2">Release Date: {{ date }}</p>
+                <div class="mt-4 p-2 text-right"></div>
             </div>
-            <p class="text-gray-500 text-sm mb-2 px-2">Release Date: {{ date }}</p>
-            <div class="mt-4 p-2 text-right">
-                <NuxtLink :to="`${type}/${id}`" class="bg-red-500 hover:bg-red-700 text-white font-medium py-2 px-4 rounded"
-                    >See more</NuxtLink
-                >
-            </div>
-        </div>
+        </NuxtLink>
     </div>
 </template>
 
--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -12,7 +12,8 @@ defineProps(["searchError"]);
                     class="border border-red-400 py-2 px-4 w-full rounded-l-full focus:outline-none focus:ring-2 focus:ring-red-400"
                     type="text"
                     placeholder="Search..."
-                    v-model.lazy="searchInput" />
+                    v-model.lazy="searchInput"
+                    @keyup.enter="$emit('searchElement', searchInput)" />
                 <button
                     ref="searchButton"
                     class="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded-r-full"
